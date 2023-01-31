660f6a0af07b9df8586aa9c1ddf9a495da33f3c1
kayanerocha <kayannerch@gmail.com>
2023-01-30 22:02:13 -0300

    protegendo a rota para criar novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -33,6 +33,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST'])
@@ -47,14 +49,16 @@ def criar():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
     if request.form['senha'] == '1234':
         session['usuario_logado'] = request.form['usuario']
         flash(f'{session["usuario_logado"]} logado com sucesso!')
-        return redirect('/novo')
+        proxima_pagina = request.form["proxima"]
+        return redirect(f'/{proxima_pagina}')
     flash('Usuário não logado!')
     return redirect('/login')
 
