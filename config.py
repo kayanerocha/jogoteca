289e7ce66c785cd52b7338a58aa4ef6eda3446d3
kayanerocha <kayannerch@gmail.com>
2023-01-31 21:49:23 -0300

    separando as configurações em outro arquivo

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'kvrds' # camada de segurança que criptografa o cookie
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = '',
+        servidor = '127.0.0.1',
+        database = 'jogoteca'
+    )
--- jogoteca.py
+++ jogoteca.py
@@ -3,18 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 # __name__: faz referência a esse próprio arquivo
 app = Flask(__name__)
-app.secret_key = 'kvrds' # camada de segurança que criptografa o cookie
-
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD = 'mysql+mysqlconnector',
-        usuario = 'root',
-        senha = '',
-        servidor = '127.0.0.1',
-        database = 'jogoteca'
-    )
+app.config.from_pyfile('config.py')
 
 # Instancia o banco do SQLAlchemy que faz a ponte com o banco de dados real
 db = SQLAlchemy(app)
 
-app.run(debug=True)
+# Importa todas as rotas
+from views import *
+
+# Faz rodar com todas as importações da aplicação
+if __name__ == '__main__':
+    app.run(debug=True)
