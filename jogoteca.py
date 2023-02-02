c9c7de9e6a94b82e85edbb444927f85b16e2bde9
kayanerocha <kayannerch@gmail.com>
2023-02-02 13:18:37 -0300

    formulario de jogo com flask form e uso de csrf_token

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 # __name__: faz referência a esse próprio arquivo
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config.from_pyfile('config.py')
 
 # Instancia o banco do SQLAlchemy que faz a ponte com o banco de dados real
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 # Importa todas as rotas
 from views import *
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
-from helpers import recupera_imagem, deleta_arquivo
+from helpers import recupera_imagem, deleta_arquivo, FormularioJogo
 import time
 
 @app.route('/')
@@ -13,13 +13,18 @@ def index():
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima='novo'))
-    return render_template('novo.html', titulo='Novo Jogo')
+    form = FormularioJogo()
+    return render_template('novo.html', titulo='Novo Jogo', form=form)
 
 @app.route('/criar', methods=['POST'])
 def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
+    form = FormularioJogo(request.form)
+    if not form.validate_on_submit():
+        return redirect(url_for('novo'))
+
+    nome = form.nome.data
+    categoria = form.categoria.data
+    console = form.categoria.data
 
     # Verifica se o jogo existe
     jogo = Jogos.query.filter_by(nome=nome).first()
@@ -44,15 +49,24 @@ def editar(id):
         return redirect(url_for('login', proxima=url_for('editar', id=id)))
     
     jogo = Jogos.query.filter_by(id=id).first()
+    form = FormularioJogo()
+    form.nome.data = jogo.nome
+    form.categoria.data = jogo.categoria
+    form.console.data = jogo.console
+
     capa_jogo = recupera_imagem(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
+    return render_template('editar.html', titulo='Editando Jogo', id=id, capa_jogo=capa_jogo, form=form)
 
 @app.route('/atualizar', methods=['POST'])
 def atualizar():
+    form = FormularioJogo(request.form)
+    if not form.validate_on_submit():
+        return redirect(url_for('index'))
+
     jogo = Jogos.query.filter_by(id=request.form['id']).first()
-    jogo.nome = request.form['nome']
-    jogo.categoria = request.form['categoria']
-    jogo.console = request.form['console']
+    jogo.nome = form.nome.data
+    jogo.categoria = form.categoria.data
+    jogo.console = form.console.data
     
     db.session.add(jogo)
     db.session.commit()
